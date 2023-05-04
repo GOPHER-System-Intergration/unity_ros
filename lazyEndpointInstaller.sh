0e196d47f7cafa3d2d8b7cbb34a65b9846714d69
Nikita Boguslavskii <bognik3@gmail.com>
2023-05-04 15:19:03 -0400

    fix(messages): Change ros_tcp_endpoint_msgs name for compatibility

--- lazyEndpointInstaller.sh
+++ lazyEndpointInstaller.sh
@@ -3,7 +3,7 @@ echo "Cloning Endpoint Repo..."
 git clone https://github.com/Unity-Technologies/ROS-TCP-Endpoint.git
 #mv ROS-TCP-Endpoint/src/ros_tcp_endpoint .
 #rm -r -f ROS-TCP-Endpoint
-cp -r unity_ros/.ros_tcp_endpoint_msgs .
-mv .ros_tcp_endpoint_msgs ros_tcp_endpoint_msgs
+cp -r unity_ros/.ROS_TCP_Endpoint_msgs .
+mv .ROS_TCP_Endpoint_msgs ROS_TCP_Endpoint_msgs
 cd ..
 catkin_make
